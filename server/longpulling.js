2d7d01cf233b667c9fe9664e26cdefbbbd69d7ab
Chupriarti <wallx13@gmail.com>
2022-01-08 19:52:39 +0300

    prepare endpoints

--- server/longpulling.js
+++ server/longpulling.js
@@ -7,4 +7,13 @@ const app = express();
 
 app.use(cors());
 
+app.get('get-messages', (req, res) => {
+    
+});
+
+app.post('new-messages', (req, res) => {
+    const message = req.body;
+    req.statusCode(200);
+});
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

3a906f31644a7a9504770fb1f7f011bbd543e3f5
Chupriarti <wallx13@gmail.com>
2022-01-09 10:16:12 +0300

    fix endpoints

--- server/longpolling.js
+++ server/longpolling.js
@@ -9,13 +9,13 @@ const app = express();
 
 app.use(cors());
 
-app.get('get-messages', (req, res) => {
+app.get('/get-messages', (req, res) => {
     emitter.once('newMessage', (message) => {
         res.json(message);
     });
 });
 
-app.post('new-messages', (req, res) => {
+app.post('/new-messages', (req, res) => {
     const message = req.body;
     emitter.emit('newMessage', message);
     req.statusCode(200);

5776f4b362d0ebc6952bb642e0a0e2f1ef3e8dcf
Chupriarti <wallx13@gmail.com>
2022-01-09 10:11:27 +0300

    prepare states

--- client/src/LongPolling.jsx
+++ client/src/LongPolling.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 
 const Longpolling = () => {
+    const [messages, setMessages] = React.useState([]);
+    const [value, setValue] = React.useState("");
+
     return (
         <div className='center'>
             <div>
                 <div className="form">
-                    <input type="text" />
+                    <input
+                        value={value}
+                        onChange={(e) => setValue(e.target.value)} 
+                        type="text" 
+                    />
                     <button>Send</button>
+                </div>
+                <div className="messages">
+                    {messages.map( mess => 
+                        <div className="message" key={mess.id}>
+                            {mess.message}
+                        </div>
+                    )}
                 </div>
             </div>
         </div>

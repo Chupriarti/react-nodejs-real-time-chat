62168912fd937cf3070253f66dc8ffc2a247e345
Chupriarti <wallx13@gmail.com>
2022-01-09 10:05:58 +0300

    prepare Longpolling component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './app.css';
+import Longpolling from './LongPolling';
 
 function App() {
   return (
     <div className="App">
-      React App
+      <Longpolling />
     </div>
   );
 }
---
+++ client/src/LongPolling.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Longpolling = () => {
+    return (
+        <div className='center'>
+            <div>
+                <div className="form">
+                    <input type="text" />
+                    <button>Send</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Longpolling;

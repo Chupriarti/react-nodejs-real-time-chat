a42790e396d8345892276fcbc52a5745c55a0f6f
Chupriarti <wallx13@gmail.com>
2022-01-11 23:18:43 +0300

    prepare websocket on listener

--- server/websocket.js
+++ server/websocket.js
@@ -3,3 +3,17 @@ const ws = require('ws');
 const wss = new ws.Server({
     port: 5000
 }, () => console.log('Server started on 5000'));
+
+wss.on('connection', function connection (ws) {
+    ws.on('message', function (message) {
+        message = JSON.parse(message);
+        switch (message.event) {
+            case 'message':
+
+                break;
+            case 'connection':
+
+                break;
+        }
+    });
+});
